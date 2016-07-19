f6ac6e1ec76c4bbad950678cf5215a8687e79ff0
Steve Nims <sjnims@gmail.com>
2016-07-19 04:55:50 +0000

    General application configuration

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,12 @@ module StarterApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    # Default Application Settings
+    config.application_name = "Starter Application"
+    config.application_creator = "Application Author"
   end
 end
