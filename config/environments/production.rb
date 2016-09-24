afdadefc347ce6821b3d71c1b9322fffe3bb7e81
Steve Nims <sjnims@gmail.com>
2016-09-24 21:41:58 +0000

    Changed Heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   # Change to app's domain name
-  host = 'shielded-river-41330.herokuapp.com'
+  host = 'still-citadel-15854.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
