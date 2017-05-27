529ff506b854b6a87956e903a4f2303d7b494214
Steve Nims <sjnims@gmail.com>
2017-05-26 22:52:59 -0400

    Changed to rails-ujs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
-//= require jquery_ujs
+//= require rails-ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
