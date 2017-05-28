8d49d4161e8105685d92b56ef33807c58e04992b
Steve Nims <sjnims@gmail.com>
2017-05-28 09:57:50 -0400

    Reverting back to using actual names in prod mailer host and domain settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = Rails.application.config.application_host
+  host = 'starter-app-rails5.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     address: 'smtp.sendgrid.net',
@@ -69,7 +69,7 @@ Rails.application.configure do
     authentication: :plain,
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: Rails.application.config.application_domain,
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
 
--- config/initializers/app_defaults.rb
+++ config/initializers/app_defaults.rb
@@ -3,9 +3,3 @@
 # Give the app a name and a creator, used in the footer view and emails
 Rails.application.config.application_name = "Starter Application"
 Rails.application.config.application_creator = "Application Author"
-
-# The following two variables seem like they should be the same and they could be
-# depending on your deployment server, however can be different if you've deployed
-# to Heroku for instance, used in the production environment mail settings
-Rails.application.config.application_host = "starter-app-rails5.herokuapp.com"
-Rails.application.config.application_domain = "heroku.com"
