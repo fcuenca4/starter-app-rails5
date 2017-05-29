cfba478fc4bfd81f90293162dd6f112c35235782
Steve Nims <sjnims@gmail.com>
2017-05-29 01:03:20 -0400

    Generalized instances where the error messages partial specified user as the object to be f.object, added a user profile test

---
+++ test/integration/users_profile_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UsersProfileTest < ActionDispatch::IntegrationTest
+  include ApplicationHelper
+
+  def setup
+    @user = users(:test_user)
+  end
+
+  test "profile display" do
+    get user_path(@user)
+    assert_template 'users/show'
+    assert_select 'title', full_title("#{@user.email}&#39;s Profile")
+    assert_select 'h1', text: @user.email
+    assert_select 'h1>img.gravatar'
+  end
+end
